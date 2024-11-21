d239d6d44f38a0ff75bbda03093a2a55e9de1ce7
Abdul Basit <abdlbasit003@gmail.com>
2024-11-21 20:38:53 +0500

    HashMap & Sets

--- src/HashMap_Sets/Solution.java
+++ src/HashMap_Sets/Solution.java
@@ -9,12 +9,14 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = "car";
-        String t = "arci";
+
 
 //        System.out.println(solution.numJewelsInStones("aA","aAAbbbb"));
 //        System.out.println(solution.containsDuplicate(new int[]{1,2,3,1}));
-        System.out.println(solution.canConstruct("aa","ab"));
+//        System.out.println(solution.canConstruct("aa","ab"));
+//        System.out.println(solution.isAnagram("anagram","nagaram"));
+//        System.out.println(solution.maxNumberOfBalloons("nlaebolko"));
+
 
     }
 
@@ -60,6 +62,7 @@ public class Solution {
     }
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character,Integer> charCount = new HashMap<>();
+
         for (char c: magazine.toCharArray()){
             if (charCount.containsKey(c)){
                 charCount.put(c,charCount.get(c)+1);
@@ -78,5 +81,69 @@ public class Solution {
         }
 
         return true;
+    }
+
+    public boolean isAnagram(String s, String t) {
+//        if(s.length() != t.length()) return false;
+//
+//        char[] sArr = s.toCharArray();
+//        char[] tArr = t.toCharArray();
+//        Arrays.sort(sArr);
+//        Arrays.sort(tArr);
+//        s = new String(sArr);
+//        t = new String(tArr);
+//
+//        return s.equals(t);
+
+        //SOL 2
+
+        Map<Character,Integer> sMap = new HashMap<>();
+        for (char c : s.toCharArray()){
+            sMap.put(c,sMap.getOrDefault(c,0)+1);
+        }
+        for (char c : t.toCharArray()){
+            if (!sMap.containsKey(c))return false;
+            if (sMap.get(c)==1)sMap.remove(c);
+            else sMap.put(c,sMap.get(c)-1);
+        }
+
+        return sMap.isEmpty();
+
+
+        // return s.chars().sorted().mapToObj(c->String.valueOf((char) c)).collect(Collectors.joining()).equals(t.chars().sorted().mapToObj(c->String.valueOf((char) c)).collect(Collectors.joining()));
+    }
+
+    public int maxNumberOfBalloons(String text) {
+
+//        int[] chars = new int[26];
+//        for(char c: text.toCharArray()){
+//            chars[c-'a']++;
+//        }
+//
+//        return Math.min(Math.min(Math.min(chars[1],chars[0]),Math.min(chars[11]/2,chars[14]/2)),chars[13]);
+
+        Map<Character,Integer> charForBal = new HashMap<>();
+        for (char c : text.toCharArray()){
+           if (c == 'b' || c=='a' || c=='l'|| c=='o'|| c=='n'){
+               charForBal.put(c,charForBal.getOrDefault(c,0)+1);
+           }
+        }
+
+        String balloon = "balloon";
+
+        for (char c: balloon.toCharArray()){
+            if (!charForBal.containsKey(c)){
+                return 0;
+            }
+        }
+
+        return Math.min(Math.min(Math.min(charForBal.get('b'),charForBal.get('a')),Math.min(charForBal.get('l')/2,charForBal.get('o')/2)),charForBal.get('n'));
+
+
+
+
+
+
+
     }
 }
